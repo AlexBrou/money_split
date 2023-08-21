2774ba52765cfbea48c9b630883ee08e7ddb5058
alexbrou <alexandrepilarbrou@gmail.com>
2023-08-21 11:56:20 +0000

    full code for Medium.com article

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # money_split
 Inspired in the Tricount suggested reimbursements
+
+This repo is used to ilustrate my Medium.com article on this topic.
--- main.py
+++ main.py
@@ -1,34 +1,121 @@
-from pydantic import BaseModel
+from pulp import (
+    LpProblem,
+    LpMinimize,
+    LpVariable,
+    LpInteger,
+    LpContinuous,
+    LpStatusOptimal,
+)
 
-USER_ALEX = "Alex"
-USER_JOANA = "Joana"
-USER_GONZALO = "Gonzalo"
+balances: dict[str, int] = {
+    "alex": 0,
+    "joana": 0,
+    "lina": 0,
+    "mischel": 0,
+    "gonzalo": 0,
+    "andre": 0,
+}
 
 
-class Purchase(BaseModel):
-    from_user: str
-    amount: float
-
-
-purchases: list[Purchase] = [
-    Purchase(from_user=USER_ALEX, amount=10),
-    Purchase(from_user=USER_JOANA, amount=2),
-    Purchase(from_user=USER_GONZALO, amount=5),
+expenses: list[tuple[int, str, str, str]] = [
+    (12, "alex", "kitchen supplies", "everyone"),
+    (10, "joana", "bathroom supplies", "everyone but gonzalo"),
+    (10, "lina", "barbecue", "everyone but andre"),
 ]
 
 
-total_pot = sum([x.amount for x in purchases])
+balances["alex"] = 10 - 2 - 2  # = 6
+balances["joana"] = -2 + 8 - 2  # = 4
+balances["lina"] = -2 - 2 + 8  # = 4
+balances["mischel"] = -2 - 2 - 2  # = -6
+balances["gonzalo"] = -2 - 0 - 2  # = -4
+balances["andre"] = -2 - 2 - 0  # = -4
 
-amount_for_each = total_pot / 3
 
-amount_alex_needs_to_pay = amount_for_each - sum(
-    [x.amount for x in purchases if x.from_user == USER_ALEX]
-)
-amount_joana_needs_to_pay = amount_for_each - sum(
-    [x.amount for x in purchases if x.from_user == USER_JOANA]
-)
-amount_gonzalo_needs_to_pay = amount_for_each - sum(
-    [x.amount for x in purchases if x.from_user == USER_GONZALO]
-)
+SMALL_O = 0.00001
 
-print(0)
+people: tuple[str, float] = [
+    ["alex", 6],
+    ["joana", 4],
+    ["lina", 4],
+    ["mishel", -6],
+    ["gonzalo", -4],
+    ["andre", -4],
+]
+
+
+assert sum([x[1] for x in people]) == 0
+
+
+problem = LpProblem("who pays who", LpMinimize)
+
+who_pays_who_variables: dict[str, LpVariable] = {}
+how_much_who_pays_who_variables: dict[str, LpVariable] = {}
+
+for person_name, _ in people:
+    for another_person_name, _ in people:
+        if person_name == another_person_name:
+            continue
+
+        who_pays_who_var = LpVariable(
+            f"who_pays_who_{person_name}_{another_person_name}",
+            lowBound=0,
+            upBound=1,
+            cat=LpInteger,
+        )
+
+        who_pays_who_variables[(person_name, another_person_name)] = who_pays_who_var
+
+        how_much_who_pays_who_var = LpVariable(
+            f"how_much_who_pays_who_{person_name}_{another_person_name}",
+            lowBound=0,
+            upBound=None,
+            cat=LpContinuous,
+        )
+
+        how_much_who_pays_who_variables[
+            (person_name, another_person_name)
+        ] = how_much_who_pays_who_var
+
+        problem += who_pays_who_var >= how_much_who_pays_who_var * SMALL_O
+
+for person, amount in people:
+    if amount < 0:
+        this_person_how_much_vars = []
+
+        for var_key in how_much_who_pays_who_variables.keys():
+            from_person, _ = var_key
+            if from_person == person:
+                this_person_how_much_vars.append(
+                    how_much_who_pays_who_variables[var_key]
+                )
+
+        problem += sum(this_person_how_much_vars) == abs(amount)
+    elif amount > 0:
+        this_person_how_much_vars = []
+
+        for var_key in how_much_who_pays_who_variables.keys():
+            _, to_person = var_key
+            if to_person == person:
+                this_person_how_much_vars.append(
+                    how_much_who_pays_who_variables[var_key]
+                )
+
+        problem += sum(this_person_how_much_vars) == abs(amount)
+
+problem.objective = sum(who_pays_who_variables.values())
+
+
+status = problem.solve()
+
+assert status == LpStatusOptimal
+
+for vvar in who_pays_who_variables.values():
+    if vvar.varValue == 0:
+        continue
+    print(f"{vvar}  ---  {vvar.varValue}")
+
+for vvar in how_much_who_pays_who_variables.values():
+    if vvar.varValue == 0:
+        continue
+    print(f"{vvar}  ---  {vvar.varValue}")
